26c6c104fa611ce8a6cfff6e84c8420344972a17
Dane Naebers <danenaebers@gmail.com>
2019-03-13 17:19:57 +0100

    rewrite entity relations

--- src/main/java/com/dane/peeper/domain/models/requestModels/PeepRequestModel.java
+++ src/main/java/com/dane/peeper/domain/models/requestModels/PeepRequestModel.java
@@ -4,13 +4,13 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class PeepRequestModel {
-    @NotNull
+    @NotEmpty
     @Size(min = 2, max = 140)
     public String text;
 }
